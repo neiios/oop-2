4f2317044bff78595d79b4971411f58bdc1cc91d
Igor <68248740+richard96292@users.noreply.github.com>
2022-02-17 18:00:41 +0200

    Implement algorithm for a single student with a C array

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
+#include <string>
+#include <iomanip>
+
+struct studentas
+{
+    std::string firstName, lastName;
+    int *grades;
+    int gradeCount, examGrade;
+    double finalGradeAvg = 0;
+};
+
+void input(studentas &s)
+{
+    std::cout << "Įveskite studento vardą: ";
+    std::cin >> s.firstName;
+    std::cout << "Įveskite studento pavardę: ";
+    std::cin >> s.lastName;
+    std::cout << "Įveskite kiek pažymių turi studentas: ";
+    std::cin >> s.gradeCount;
+
+    s.grades = new int[s.gradeCount];
+    for (size_t i = 0; i < s.gradeCount; i++)
+    {
+        std::cout << "Įveskite pažymį Nr." << i + 1 << ": ";
+        std::cin >> s.grades[i];
+        s.finalGradeAvg += s.grades[i];
+    }
+
+    std::cout << "Įveskite studento egzamino pažymį: ";
+    std::cin >> s.examGrade;
+    s.finalGradeAvg = (s.finalGradeAvg / s.gradeCount) * 0.4 + s.examGrade * 0.6;
+}
+
+void output(studentas s)
+{
+    std::cout << std::left << std::setw(15) << "Pavardė" << std::setw(15) << "Vardas" << std::setw(15) << "Galutinis (Vid.)\n";
+    std::cout << "----------------------------------------------------------\n";
+    std::cout << std::left << std::setw(15) << s.firstName << std::setw(15) << s.lastName << std::setw(15) << std::fixed << std::setprecision(2) << s.finalGradeAvg << "\n";
+}
 
 int main()
 {
-    std::cout << "Hello World" << std::endl;
+    studentas s;
+    input(s);
+    output(s);
     return 0;
 }
